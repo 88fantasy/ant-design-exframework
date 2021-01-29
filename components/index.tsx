65bbc69d01cd9ff91a38bbca0d4b0af1498c4396
88fantasy <88fantasy@gmail.com>
2021-01-29 16:18:40 +0800

    add permission

--- components/index.tsx
+++ components/index.tsx
@@ -4,6 +4,7 @@ export { default as QueryParamBar } from './QueryParamBar';
 export { default as RouterMenu } from './RouterMenu';
 export { default as Hov } from './Hov';
 export { default as QueryParamModal } from './QueryParamModal';
+export { default as Permission } from './Permission';
 
 export { 
   PaginationPage, 
